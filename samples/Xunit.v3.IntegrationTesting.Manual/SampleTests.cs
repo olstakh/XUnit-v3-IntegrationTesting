081682eb022b1f6bba07527005d5aae9f35692ac
Alex S <olstakh@microsoft.com>
2025-08-26 13:42:06 -0700

    Update samples

--- samples/Xunit.v3.IntegrationTesting.Manual/SampleTests.cs
+++ samples/Xunit.v3.IntegrationTesting.Manual/SampleTests.cs
@@ -1,33 +1,30 @@
 ﻿using Xunit;
 using Xunit.v3;
-using Fact = Xunit.v3.IntegrationTesting.FactDependsOnAttribute;
-
-namespace Xunit.v3.IntegrationTesting.Manual;
 
 public class IntegrationTests
 {
-    [@Fact]
+    [FactDependsOn]
     public void Test_DatabaseSetup()
     {
         // Setup database
         Assert.False(true);
     }
 
-    [@Fact(Dependencies = [nameof(Test_DatabaseSetup)])]
+    [FactDependsOn(Dependencies = [nameof(Test_DatabaseSetup)])]
     public void Test_CreateUser()
     {
         // Create user - depends on database setup
         Assert.True(true);
     }
 
-    [@Fact]
+    [FactDependsOn]
     public void Test_UserLogin()
     {
         // Test user login - depends on user creation
         Assert.True(true);
     }
 
-    [@Fact(Dependencies = [nameof(Test_UserLogin), nameof(Test_CreateUser)])]
+    [FactDependsOn(Dependencies = [nameof(Test_UserLogin), nameof(Test_CreateUser)])]
     public void Test_UserProfile()
     {
         // Test user profile - depends on both user creation and login
