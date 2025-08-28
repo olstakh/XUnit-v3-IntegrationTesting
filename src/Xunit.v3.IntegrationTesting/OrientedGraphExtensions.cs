1fb581ee07253fdde6b4a3be0be360ad1de31711
Alex S <olstakh@microsoft.com>
2025-08-27 17:35:54 -0700

    Added oriented graph extensions tests

--- src/Xunit.v3.IntegrationTesting/OrientedGraphExtensions.cs
+++ src/Xunit.v3.IntegrationTesting/OrientedGraphExtensions.cs
@@ -34,7 +34,7 @@ public static class OrientedGraphExtensions
                             "Multiple tests found with the same name '{0}' in class '{1}'. Total test cases: '{2}'. This is not allowed.",
                             dependency,
                             testCase.TestClassName,
-                            string.Join(", ", dependentTest.Select(t => $"{t.TestClassName}.{t.TestMethodName}"))));
+                            dependentTest.Count()));
                 }
                 if (dependentTest.Count > 0)
                 {
